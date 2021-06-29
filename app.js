d21f4930e5862ed339beec253b893ec4df05a49f
IkTom22 <ikotominaga@gmail.com>
2021-06-29 14:57:56 +1200

    finished

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require("express");
+
 const app = express();
 const port = 5000;
-app.set("views engine", "ejs");
+
+// Static files
+// __dirname is root directory
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use("/js", express.static(__dirname + "public/js"));
+
+//Templating engine
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+//Routes - homepage , which is '/' is using newsRouter
+const newsRouter = require("./src/routes/news");
+
+app.use("/", newsRouter);
+app.use("/article", newsRouter);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
